d3889ad99213d307170520041a78f49842e18298
Pyee24 <ben.metcalf@outlook.com>
2026-03-02 17:14:39 +0000

    Set up the work and added a systme usage method

--- process_manager.sh
+++ process_manager.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+system_usage()
+{
+    echo " CPU and Memory Usage"
+    echo
+    echo "CPU Load"
+    uptime
+    echo
+    echo "Memory usage"
+    echo
+    free -h
+    echo
+    log_action "Displayed CPU and Memory usage"
+}
+
+system_usage
