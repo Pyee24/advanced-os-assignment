3832d6ab6bab88bd4b4084aee34d26aa3d5c1816
Pyee24 <ben.metcalf@outlook.com>
2026-03-04 20:48:34 +0000

    disk inspection and archives process added

--- process_manager.sh
+++ process_manager.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 critical_pid = ("1" "$$")
+ARCHIVE_DIR = "ArchiveLogs"
+SIZE_LIMIT = $((50*1024*1024))
+ARCHIVE_MAX = $((1024*1024*1024))
 
 show_cpu_memory()
 {
@@ -38,6 +41,47 @@ terminate_process()
         *)
             echo "Cancelled" ;;
     esac
+}
+
+Disk_inspection_archive()
+{
+    read -p "Enter directory for inspection" dir
+
+    if [[ ! -d "$dir"]]; then
+        echo "Directory not found"
+        return
+    fi
+
+    echo "Disk usage for $dir"
+    du -sh "$dir"
+
+    mkdir -p "$ARCHIVE_DIR"
+
+    echo "Logging large files in excess of 50mb in $dir directory"
+    mapfile -t large_files <(find "$dir" -type f -name "*.log" -size +" ${SIZE_LIMIT}c" 2>/dev/null)
+
+    if [[ ${#large_files[@]} -eq 0]] then
+        echo "No large log files in $dir"
+        return
+    fi
+
+    for f in "${large_files[@]}"; do
+        echo "Compressing $f"
+        base=$(basename "$f")
+        ts=$(date '+%Y%m%d_%H%M%S')
+        tar_name="${ARCHIVE_DIR}/${base}_${ts}.tar.gz"
+        tar -czf "$tar_name" "$f"
+    done
+
+    archive_size=$(du -sb "$ARCHIVE_DIR" | awk '{print $1}')
+
+    if ((archive size > ARCHIVE_MAX)); then
+        echo "WARNING Archive Log exceeds 1GB , current size ${archive_size} bytes"
+    fi
+
+
+
+
 }
 
 show_cpu_memory
