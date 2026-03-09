867464c484264d93edc95f17dfffb869062d3db7
Pyee24 <ben.metcalf@outlook.com>
2026-03-09 19:24:20 +0000

    submission ; checker and list complete

--- examination_asc.sh
+++ examination_asc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SUBMISSION_DIR="submissions"
+LOG_SUBMISSIONS="submission_log.txt"
 
 
 
@@ -40,12 +41,52 @@ check_duplicates()
     local file="$1"
     local filename=$(basename "$file")
 
-    if [[ -f "SUBMISSION_DIR/$filename"]]; then
-        if cmp -s "$file" "SUBMISSION_DIR/$filename"]]; then
+    if [[ -f "$SUBMISSION_DIR/$filename"]]; then
+        if cmp -s "$file" "$SUBMISSION_DIR/$filename"]]; then
             echo "Duplicate files detected."
             return 1
         fi
     fi
 
     return 0
+}
+
+assignment_submission()
+{
+    read -p "Enter File path: " file
+    
+    file_validation "$file" || return
+    check_duplicates "$file" || return
+
+    filename=$(basename "$file")
+    cp "$file" "$SUBMISSION_DIR/$filename" 
+
+    echo "$(date): Submitted $filename" >> "$LOG_SUBMISSIONS"
+    echo "Successful Submission"
+
+
+
+}
+
+submission_check()
+{
+    read -p "Enter name of file to check: " filename
+
+    if [[ -f $SUBMISSION_DIR/$filename]]; then
+        echo "File has been submitted"
+    else
+        echo "File has not been submitted"
+    fi
+}
+
+list_submissions()
+{
+    echo "Submission List"
+    ls -1 "$SUBMISSION_DIR"
+
+}
+
+login_simulation()
+{
+    
 }
